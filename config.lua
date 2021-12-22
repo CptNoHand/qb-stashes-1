fe51116699629d6132a330ad1e369a57f645e018
CptNoHand <90973107+CptNoHand@users.noreply.github.com>
2021-12-22 02:16:14 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -3,16 +3,16 @@ Config = {}
 Config.PoliceOpen = true -- Can Police open all stashes
 
 Config.Stashes = {
-    -- ["geminimansion"] = {
-    --     stashName = "geminimansion",
-    --     coords = vector3(-1730.5, 358.6, 88.73), 
-    --     requirecid = true,
-    --     jobrequired = false,
-    --     gangrequired = false,
-    --     gang = "",
-    --     job = "",
-    --     cid = {"MAH44286"},  
-    --     stashSize = 1250000,
-    --     stashSlots = 125, 
-    -- },
+    ["aydenhouse"] = {
+        stashName = "Ayden House",
+        coords = vector3(-265.47, -733.64, 125.47), 
+        requirecid = true,
+        jobrequired = false,
+        gangrequired = false,
+        gang = "",
+        job = "",
+        cid = {"UCC99033"},  
+        stashSize = 250000,
+        stashSlots = 100, 
+    },
 }
